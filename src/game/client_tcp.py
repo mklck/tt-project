64795ed49901c606869f9bd150a0c9eae4490fa4
mklck <mklck@interia.pl>
2024-12-09 20:04:15 +0100

    Add GUI screenshot

--- src/game/client_tcp.py
+++ src/game/client_tcp.py
@@ -16,17 +16,9 @@ Color = Enum(
 )
 
 class ClientTCP:
-<<<<<<< HEAD
-    
-        def __init__(self):
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-            self.crypto_client = crypto.cryptographic(32);
-            self.conn = 0;
-=======
 	def __init__(self):
 		self.crypto_client = crypto.cryptographic(32);
 		self.conn = None
->>>>>>> 851b831185f6c1ad7b8f5466f4c7a9c940071e63
 
 	def receive(self):
 		try:
@@ -37,18 +29,6 @@ class ClientTCP:
 			print("Connection lost")
 			self.s.close()
 
-<<<<<<< HEAD
-                                event.set();
-                                board.put(received_board);
-                        except:
-                                print("Brak polaczenia");
-                                self.s.close();
-                                lock.release();
-                                self.conn = 0;
-                                return;
-                
-=======
->>>>>>> 851b831185f6c1ad7b8f5466f4c7a9c940071e63
 
 	def send(self, data : bytes):
 		try:
@@ -58,37 +38,8 @@ class ClientTCP:
 			print("Connection lost");
 			self.s.close();
 
-<<<<<<< HEAD
-                                # convert each item to int type
-                                for i in range(len(board)):
-                                        board[i] = int(board[i])
-                                        
-                                queue.get(); # just for remove
-                                queue.put(board);
-                                print("Wysłana plansza: ", *board );
-                                data = bytes(board);
-
-                                data_encrypted = self.crypto_client.encrypt( data );
-
-                                try:
-                                        self.s.send( data_encrypted );
-                                        event.clear();
-                                except:
-                                        print("Brak polaczenia");
-                                        self.s.close();
-                                        lock.release();
-                                        self.conn = 0;
-                                        return;
-                        else:
-                                print("Oczekiwanie na ruch gracz (...) ");
-                                time.sleep(0.5);
-
-        def exit(self):
-            self.s.close();        
-=======
 	def exit(self):
 		self.s.close()
->>>>>>> 851b831185f6c1ad7b8f5466f4c7a9c940071e63
              
 	def connect_serwer(self, TCP_IP = '127.0.0.1', TCP_PORT = 5005, BUFFER_SIZE = 20000):
 		try:
@@ -128,24 +79,9 @@ class ClientTCP:
 					)
 					conn_state = Color.SERVER_KEY_ACK;
                         
-<<<<<<< HEAD
-                    case Color.SERVER_KEY_ACK:
-                         print("Waiting for publick key ACK from serwer")
-                         data = self.s.recv(BUFFER_SIZE);
-                         
-                         if data.decode()[0:7] == "KEY_ACK":
-                            print("ACK Got")
-                            self.conn = 1;
-                            return
-
-
-
-
-=======
 				case Color.SERVER_KEY_ACK:
 					print("Waiting for publick key ACK from serwer")
 					data = self.s.recv(BUFFER_SIZE);
 					if data.decode()[0:7] == "KEY_ACK":
 						print("ACK Got")
 						return
->>>>>>> 851b831185f6c1ad7b8f5466f4c7a9c940071e63
--- src/game/main.py
+++ src/game/main.py
@@ -1,47 +1,4 @@
-<<<<<<< HEAD
-import socket
-import time
-import crypto
-from enum import Enum
-from dataclasses	import dataclass
-from _thread import *
-import threading
-import queue as q
-import server_tcp
-
-
-
-if __name__== "__main__":
-    server = server_tcp.SerwerTCP();
-    server.bind_serwer();
-    
-    
-    while 1:
-        conn, addr = server.s.accept()
-        event = threading.Event();
-        conn = server.connection(conn, event);
-        print('Connection address:', addr)
-
-        #zdefiniowania watkow wysylanie/odbieranie
-        print_thread = threading.Lock();
-        read_thread = threading.Lock();
-
-        # Plansza do gry
-        board = q.Queue();
-        board.put([0,0,0,0,0,0,0,0,0])
-
-        # uruchomienie watkow
-        print_thread.acquire();
-        start_new_thread(server.send, (conn,print_thread, event, board))
-            
-        read_thread.acquire();
-        start_new_thread(server.receive, (conn,read_thread, event, board))
-        
-        
-        
-=======
 from .game import Game
->>>>>>> 851b831185f6c1ad7b8f5466f4c7a9c940071e63
 
 def main():
 	Game().run()
